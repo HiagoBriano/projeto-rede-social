833ac925b8e9e79b7b309d865afa4fbe66e9069d
Hiago Briano <hiago.artist@hotmail.com>
2023-03-08 22:57:20 -0300

    tirei crases do SQL

--- API/sql/sql.sql
+++ API/sql/sql.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE IF NOT EXISTS `Devbook`
-USE `Devbook`;
+CREATE DATABASE IF NOT EXISTS Devbook;
+USE Devbook;
+DROP TABLE IF EXISTS users;
 
-DROP TABLE IF EXISTS `Users`;
-
-CREATE TABLE `Users` (
-  `id` int AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(50) NOT NULL,
-  `nickname` varchar(50) NOT NULL UNIQUE,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `password` varchar(255) NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP()
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  nickname varchar(50) NOT NULL UNIQUE,
+  email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE=InnoDB;
